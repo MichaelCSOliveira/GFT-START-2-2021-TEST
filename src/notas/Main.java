eb32b96dd4c081b9f614a242df413c3075c0cfc4
MichaelCSOliveira <michaelcsoliveira@gmail.com>
2021-08-19 18:03:36 -0300

    Teste GFT

--- src/notas/Main.java
+++ src/notas/Main.java
@@ -1,9 +1,25 @@
 package notas;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner reader = new Scanner (System.in);
+		
+		String nome[] = {"Dó", "Ré", "Mi", "Fá", "Sol", "Lá", "Si"};
+			
+		System.out.print("Digite o número inicial: ");
+		int init = reader.nextInt();
+		System.out.print("Digite o número final: ");
+		int fin = reader.nextInt();
+		int rest = fin;
+		while (rest>0) {
+			for (int i = init; i <= 6; i++) {
+				System.out.print(nome[i]+" ");
+			}
+		}
 
 	}
 
